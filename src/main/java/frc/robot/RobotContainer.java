54e215dd8b35f561d9dddd9eccc97f84966cc74b
Jeff Lange <jeff_lange@vuzix.com>
2025-02-05 21:06:11 -0500

    reefcontrol placeholder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,10 +18,13 @@ import static frc.robot.subsystems.vision.VisionConstants.*;
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
@@ -122,6 +125,23 @@ public class RobotContainer {
     autoChooser.addOption(
         "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    var table = inst.getTable("ReefControl");
+    table.getEntry(".type").setString("ReefControl");
+    table.getEntry("coralLoaded").setBoolean(false);
+    table.getEntry("algaeLoaded").setBoolean(false);
+    table.getEntry("executeCommand").setBoolean(false);
+
+    // inst.top
+    // TopicInfo reefTopic = new TopicInfo(
+    //     NetworkTableInstance.getDefault(),
+    //      0, camera1Name, 0, camera0Name)
+    SmartDashboard.putData(
+        "Test Command",
+        new InstantCommand(
+            () -> {
+              System.out.println("Test Command");
+            }));
     // Configure the button bindings
     configureButtonBindings();
   }
