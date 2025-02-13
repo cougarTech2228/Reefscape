d337c701356b547986bb4a02c7f448cba9c1360a
Cookie Monster <cougartechsoftware@gmail.com>
2025-02-12 19:32:43 -0500

    All blue reef points with zones added to Destination

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,6 +27,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.Destination;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.algaeAcquirer.AlgaeAcquirer;
@@ -55,188 +56,198 @@ import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // Subsystems
-  private final Drive drive;
-  private final Elevator elevator;
-  private final CoralCone coralCone;
-  private final AlgaeAcquirer algaeAcquirer;
+    // Subsystems
+    private final Drive drive;
+    private final Elevator elevator;
+    private final CoralCone coralCone;
+    private final AlgaeAcquirer algaeAcquirer;
 
-  @SuppressWarnings("unused")
-  private final Vision vision;
+    @SuppressWarnings("unused")
+    private final Vision vision;
 
-  @SuppressWarnings("unused")
-  private final OperatorUI operatorUI;
+    @SuppressWarnings("unused")
+    private final OperatorUI operatorUI;
 
-  
+    // Controller
+    private final CommandXboxController driverController = new CommandXboxController(0);
+    private final Joystick buttonBox1 = new Joystick(1);
+    private final Joystick buttonBox2 = new Joystick(2);
+    private final ButtonBoard buttonBoard;
 
-  // Controller
-  private final CommandXboxController driverController = new CommandXboxController(0);
-  private final Joystick buttonBox1 = new Joystick(1);
-  private final Joystick buttonBox2 = new Joystick(2);
-  private final ButtonBoard buttonBoard;
+    // Dashboard inputs
+    private final LoggedDashboardChooser<Command> autoChooser;
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        switch (Constants.currentMode) {
+            case REAL:
+                // Real robot, instantiate hardware IO implementations
+                drive = new Drive(
+                        new GyroIOPigeon2(),
+                        new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                        new ModuleIOTalonFX(TunerConstants.FrontRight),
+                        new ModuleIOTalonFX(TunerConstants.BackLeft),
+                        new ModuleIOTalonFX(TunerConstants.BackRight));
+                vision = new Vision(
+                        drive::addVisionMeasurement,
+                        new VisionIOPhotonVision(camera0Name, robotToCamera0),
+                        new VisionIOPhotonVision(camera1Name, robotToCamera1));
+                elevator = new Elevator(new ElevatorIOTalonFX());
+                algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIONeo());
+                coralCone = new CoralCone(new CoralConeIONeo());
+                break;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
-        vision =
-            new Vision(
-                drive::addVisionMeasurement,
-                new VisionIOPhotonVision(camera0Name, robotToCamera0),
-                new VisionIOPhotonVision(camera1Name, robotToCamera1));
-        elevator = new Elevator(new ElevatorIOTalonFX());
-        algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIONeo());
-        coralCone = new CoralCone(new CoralConeIONeo());
-        break;
+            case SIM:
+                // Sim robot, instantiate physics sim IO implementations
+                drive = new Drive(
+                        new GyroIO() {
+                        },
+                        new ModuleIOSim(TunerConstants.FrontLeft),
+                        new ModuleIOSim(TunerConstants.FrontRight),
+                        new ModuleIOSim(TunerConstants.BackLeft),
+                        new ModuleIOSim(TunerConstants.BackRight));
+                vision = new Vision(
+                        drive::addVisionMeasurement,
+                        new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, drive::getPose),
+                        new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose));
+                elevator = new Elevator(new ElevatorIOTalonFXSim());
+                algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIONeoSim());
+                coralCone = new CoralCone(new CoralConeIONeoSim());
+                break;
 
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
-        vision =
-            new Vision(
-                drive::addVisionMeasurement,
-                new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, drive::getPose),
-                new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose));
-        elevator = new Elevator(new ElevatorIOTalonFXSim());
-        algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIONeoSim());
-        coralCone = new CoralCone(new CoralConeIONeoSim());
-        break;
+            default:
+                // Replayed robot, disable IO implementations
+                drive = new Drive(
+                        new GyroIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        });
+                vision = new Vision(drive::addVisionMeasurement, new VisionIO() {
+                }, new VisionIO() {
+                });
 
-      default:
-        // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        vision = new Vision(drive::addVisionMeasurement, new VisionIO() {}, new VisionIO() {});
+                elevator = new Elevator(new ElevatorIO() {
+                });
+                algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIO() {
+                });
+                coralCone = new CoralCone(new CoralConeIO() {
+                });
+                break;
+        }
 
-        elevator = new Elevator(new ElevatorIO(){});
-        algaeAcquirer = new AlgaeAcquirer(new AlgaeAcquirerIO(){});
-        coralCone = new CoralCone(new CoralConeIO(){});
-        break;
+        operatorUI = new OperatorUI(elevator, algaeAcquirer, coralCone);
+
+        // Set up auto routines
+        autoChooser = new LoggedDashboardChooser<>("Auto Choices"/* , AutoBuilder.buildAutoChooser() */);
+
+        // Set up SysId routines
+        autoChooser.addOption(
+                "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+        autoChooser.addOption(
+                "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+        autoChooser.addOption(
+                "Drive SysId (Quasistatic Forward)",
+                drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption(
+                "Drive SysId (Quasistatic Reverse)",
+                drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        autoChooser.addOption(
+                "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption(
+                "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+        autoChooser.addOption(
+                "Elevator SysId (Quasistatic Forward)",
+                elevator.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption(
+                "Elevator SysId (Quasistatic Reverse)",
+                elevator.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        autoChooser.addOption(
+                "Elevator SysId (Dynamic Forward)",
+                elevator.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption(
+                "Elevator SysId (Dynamic Reverse)",
+                elevator.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+        SmartDashboard.putData(
+                "Test Command",
+                new InstantCommand(
+                        () -> {
+                            System.out.println("Test Command");
+                        }));
+        // Configure the button bindings
+        configureButtonBindings();
+        buttonBoard = new ButtonBoard(buttonBox1, buttonBox2, elevator, coralCone, algaeAcquirer);
+        buttonBoard.configureButtonBindings();
+
     }
 
-    operatorUI = new OperatorUI(elevator, algaeAcquirer, coralCone);
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        // Default command, normal field-relative drive
+        drive.setDefaultCommand(
+                DriveCommands.joystickDrive(
+                        drive,
+                        () -> -driverController.getLeftY(),
+                        () -> -driverController.getLeftX(),
+                        () -> -driverController.getRightX()));
 
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices"/*, AutoBuilder.buildAutoChooser()*/);
+        // Lock to 0° when A button is held
+        driverController
+                .a()
+                .whileTrue(
+                        DriveCommands.joystickDriveAtAngle(
+                                drive,
+                                () -> -driverController.getLeftY(),
+                                () -> -driverController.getLeftX(),
+                                () -> new Rotation2d()));
 
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+        // Switch to X pattern when X button is pressed
+        driverController.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    autoChooser.addOption(
-        "Elevator SysId (Quasistatic Forward)",
-        elevator.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Elevator SysId (Quasistatic Reverse)",
-        elevator.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Elevator SysId (Dynamic Forward)",
-        elevator.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Elevator SysId (Dynamic Reverse)",
-        elevator.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+        // Reset gyro to 0° when B button is pressed
+        driverController
+                .b()
+                .onTrue(
+                        Commands.runOnce(
+                                () -> drive.setPose(
+                                        new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                                drive)
+                                .ignoringDisable(true));
+    }
 
-    SmartDashboard.putData(
-        "Test Command",
-        new InstantCommand(
-            () -> {
-              System.out.println("Test Command");
-            }));
-    // Configure the button bindings
-    configureButtonBindings();
-    buttonBoard = new ButtonBoard(buttonBox1, buttonBox2, elevator, coralCone, algaeAcquirer);
-    buttonBoard.configureButtonBindings();
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return DriveCommands.shortAutoPath(drive, Destination.BLUE_6_RIGHT);
+        // return autoChooser.get();
+    }
 
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> -driverController.getLeftY(),
-            () -> -driverController.getLeftX(),
-            () -> -driverController.getRightX()));
-
-    // Lock to 0° when A button is held
-    driverController
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -driverController.getLeftY(),
-                () -> -driverController.getLeftX(),
-                () -> new Rotation2d()));
-
-    // Switch to X pattern when X button is pressed
-    driverController.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
-
-    // Reset gyro to 0° when B button is pressed
-    driverController
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autoChooser.get();
-  }
-
-  
 }
