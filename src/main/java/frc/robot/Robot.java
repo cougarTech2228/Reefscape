40a9941b5513a4ecc86406dd6d01baa4c7b71a3b
Jeff Lange <jlange6648@gmail.com>
2025-02-25 18:46:15 -0500

    Update phoenix6 and pathplanner

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -148,7 +148,7 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    robotContainer.calculateMaxAcceleration();
+    // robotContainer.calculateMaxAcceleration();
   }
 
   /** This function is called once when teleop is enabled. */
