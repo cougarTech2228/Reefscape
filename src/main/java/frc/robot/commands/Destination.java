b3bc64874b0e38e5272cbf8d83f0048849ab3afa
White Laptop <cougartechsoftware@gmail.com>
2025-03-11 19:27:36 -0400

    Larger coral station zones

--- src/main/java/frc/robot/commands/Destination.java
+++ src/main/java/frc/robot/commands/Destination.java
@@ -47,13 +47,13 @@ public enum Destination {
 
     PROCESSOR(16, 0, 0.1, 6.5, 0.55, 7.3, 2, 5, 2, 5.6, 0.55),
 
-    LOADER_RIGHT_LEFT(12, DestConsts.loaderLeftOffset, DestConsts.loaderXOffset, 1.7, 0.55, 2.9, 1.3, 1.2, 2.6, 0.1, 1.2),
-    LOADER_RIGHT_CENTER(12, DestConsts.loaderCenterOffset, DestConsts.loaderXOffset, 1.7, 0.55, 2.9, 1.3, 1.2, 2.6, 0.1, 1.2),
-    LOADER_RIGHT_RIGHT(12, DestConsts.loaderRightOffset,DestConsts.loaderXOffset, 1.7, 0.55, 2.9, 1.3, 1.2, 2.6, 0.1, 1.2),
+    LOADER_RIGHT_LEFT(12, DestConsts.loaderLeftOffset, DestConsts.loaderXOffset, 0.5,0,6,0,0,4,0,0.5),
+    LOADER_RIGHT_CENTER(12, DestConsts.loaderCenterOffset, DestConsts.loaderXOffset, 0.5,0,6,0,0,4,0,0.5),
+    LOADER_RIGHT_RIGHT(12, DestConsts.loaderRightOffset,DestConsts.loaderXOffset, 0.5,0,6,0,0,4,0,0.5),
 
-    LOADER_LEFT_LEFT(13, DestConsts.loaderLeftOffset, 0, 0.3, 6.8, 1, 5, 3, 6.5, 1.7, 8),
-    LOADER_LEFT_CENTER(13, DestConsts.loaderCenterOffset, 0, 0.3, 6.8, 1, 5, 3, 6.5, 1.7, 8),
-    LOADER_LEFT_RIGHT(13, DestConsts.loaderRightOffset, 0, 0.3, 6.8, 1, 5, 3, 6.5, 1.7, 8),
+    LOADER_LEFT_LEFT(13, DestConsts.loaderLeftOffset, 0, 4,0,6,8,0.5,8,0,7.5),
+    LOADER_LEFT_CENTER(13, DestConsts.loaderCenterOffset, 0, 4,0,6,8,0.5,8,0,7.5),
+    LOADER_LEFT_RIGHT(13, DestConsts.loaderRightOffset, 0, 4,0,6,8,0.5,8,0,7.5),
 
     BARGE (14, 0, 0, 8.2, 7.4, 6.5, 7.4, 6.5, 4.8, 8.2, 4.8);
 
