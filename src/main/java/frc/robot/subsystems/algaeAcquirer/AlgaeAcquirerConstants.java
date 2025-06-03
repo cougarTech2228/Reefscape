ca652b740b3ef66ff04a957d972ecc93595c00d0
Cookie Monster <cougartechsoftware@gmail.com>
2025-06-02 21:38:50 -0400

    Change Algae load speed threshold
    raise the minimum speed threshold to better detect when algae is loaded

--- src/main/java/frc/robot/subsystems/algaeAcquirer/AlgaeAcquirerConstants.java
+++ src/main/java/frc/robot/subsystems/algaeAcquirer/AlgaeAcquirerConstants.java
@@ -20,7 +20,7 @@ public class AlgaeAcquirerConstants {
     public static final double kManualDownVoltage = -0.5;
     
     // below this velocity and above this current will be concidered loaded
-    public static final double kLoadedVelocityThreshold = 1000;
+    public static final double kLoadedVelocityThreshold = 2000;
     public static final double kLoadedCurrentDrawThreshold = 5;
 
     public enum FlyMode {
